4745c7dfafe2e1b9f46943c2e87f430662b9ca30
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-02-13 16:32:40 +0200

    hw
    hw

---
+++ src/js/renderCountries.js
@@ -0,0 +1,40 @@
+import { Notify } from "notiflix";
+export default function renderCountries(dataCountries) {
+    const countryList = document.querySelector('.country-list');
+    const countryInfo = document.querySelector('.country-info');
+    let quantityCountries = dataCountries.length;
+    let currentCountry = dataCountries[0];
+
+    let countryItems = [];
+
+    if (quantityCountries > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.'
+        );
+    } else if (quantityCountries >= 2 && quantityCountries <= 10) {
+         countryItems = dataCountries
+            .map(country =>
+                `<li class="large-flag_item">
+                <img src="${country.flags.svg}" alt="" class="flags" />
+                <span>${country.name.official}</span>
+            </li>`).join(' ');
+            
+        countryInfo.innerHTML = "";
+        countryList.innerHTML = countryItems;
+        
+    } else {
+        countryInfo.innerHTML = `
+        <div class="large_flag">
+            <img src="${currentCountry.flags.svg}" alt="" class="flags" />
+            <span class="country">${currentCountry.name.official}</span>
+        </div>
+        <div>
+            <p>Capital: ${currentCountry.capital} </p>
+            <p>Population: ${currentCountry.population} </p>
+            <p>Languages: ${Object.values(currentCountry.languages)
+            .join(', ')} 
+                </p>
+        </div>`;
+        countryList.innerHTML = "";
+    }
+}
+
